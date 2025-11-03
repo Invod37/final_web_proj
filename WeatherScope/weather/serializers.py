bb89b2f7668d3b480b62e7d3a1996fb1d5044833
User <egorkathebestlordtop1gg@gmail.com>
2025-11-04 00:03:38 +0200

    clear

--- WeatherScope/weather/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Like
-
-class LikeSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Like
-        fields = ['id', 'city_name', 'liked_at']
-        read_only_fields = ['id', 'liked_at']
